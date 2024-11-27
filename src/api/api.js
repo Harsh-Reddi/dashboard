002ea50d360332d3653229365ac9237e2ba3cb39
Harsh-Reddi <Harshithareddi14@gmail.com>
2024-11-27 14:57:13 -0500

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL : 'https://backend-n789.onrender.com/api'
+    baseURL : 'https://backend-n789.onrender.com/api',
+    withCredentials: true,
 })
 
 export default api
